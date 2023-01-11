b109d7fc832f8889ab0ddf2e711a733ca028f51a
Ryan Wilson <92859295+MovByte@users.noreply.github.com>
2023-01-10 21:31:48 -0500

    Apply flags to the Dynamic Update listener

--- updates.js
+++ updates.js
@@ -1,17 +1,18 @@
 import handleStore from "./util/handleStore.js";
 
-import { dynamicConfig } from "./config.js";
+import { dynamicConfig, flags } from "./config.js";
 
 const { dbName, id } = dynamicConfig;
 
 // For dynamic config updates
 export default () => {
-	handleStore(dbName, store => {
-		self.addEventListener("message", event => {
-			const config = event.data;
+	if (flags.dynamicConfig)
+		handleStore(dbName, store => {
+			self.addEventListener("message", event => {
+				const config = event.data;
 
-			if (typeof config === "object" && config.id === id)
-				store.add(config);
+				if (typeof config === "object" && config.id === id)
+					store.add(config);
+			});
 		});
-	});
 };
