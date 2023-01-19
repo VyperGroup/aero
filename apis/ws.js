cd3a57f09d80457fdce6bcfaae1db1489b7deeff
Ryan Wilson <92859295+MovByte@users.noreply.github.com>
2023-01-18 21:10:25 -0500

    Better logging in WS interceptor

--- apis/ws.js
+++ apis/ws.js
@@ -1,12 +1,12 @@
 WebSocket = new Proxy(WebSocket, {
 	construct(target, args) {
-		console.log("Intercepting Websocket connection");
-
 		[url] = args;
 
 		const rewrittenUrl = `${
 			location.protocol === "https:" ? "wss" : "ws"
 		}://${location.host}/${$aero.config.proxyApiWs}?url=${url}`;
+
+		console.log(`WS ${url} -> ${rewrittenUrl}`);
 
 		args[0] = rewrittenUrl;
 
