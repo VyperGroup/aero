2fa135e9c5f017a02d2afdf1dea79ad03cd069a5
Ryan Wilson <92859295+MovByte@users.noreply.github.com>
2023-01-22 22:12:21 -0500

    Stabilized scoping API interceptors

--- apis/scoping.js
+++ apis/scoping.js
@@ -3,54 +3,45 @@ if ($aero.config.flags.advancedScoping) {
 	$aero.isLocation = val => val === location;
 
 	$aero.check = val => (val == location ? $aero.location : val);
-
-	$aero.eval = new Proxy(eval, {
-		apply(target, that, args) {
-			args[0] = $aero.scope(args[0]);
-
-			return Reflect.apply(...arguments);
-		},
-	});
-
-	Function = new Proxy(Function, {
-		construct(target, args) {
-			[func] = args;
-
-			let bak = "";
-
-			if (typeof func === "string") {
-				bak = func;
-				func = $aero.scope(func);
-			} else if (
-				typeof func === "function" &&
-				!func.toString() !==
-					`function ${func.name}() { [native code] }"`
-			) {
-				bak = func.toString();
-				func = $aero.scope(func.toString());
-			}
-
-			args[0] = func;
-
-			const inst = Reflect.construct(...arguments);
-
-			inst.bak = bak;
-
-			return inst;
-		},
-	});
-
-	/*
-	Function.prototype.toString = new Proxy(Function.prototype.toString, {
-		apply: (target, that, args) => {
-			//console.log(target);
-			console.log(that);
-			//console.log(args);
-			return that.bak
-		},
-	});
-	*/
 }
+
+$aero.eval = new Proxy(eval, {
+	apply(target, that, args) {
+		args[0] = $aero.scope(args[0]);
+
+		return Reflect.apply(...arguments);
+	},
+});
+
+Function = new Proxy(Function, {
+	construct(target, args) {
+		[func] = args;
+
+		let bak = "";
+
+		if (typeof func === "string") {
+			bak = func;
+			func = $aero.scope(func);
+		} else if (
+			typeof func === "function" &&
+			!func.toString() !== `function ${func.name}() { [native code] }"`
+		) {
+			bak = func.toString();
+			func = $aero.scope(func.toString());
+		}
+
+		args[0] = func;
+
+		const inst = Reflect.construct(...arguments);
+
+		inst.bak = bak;
+
+		// Hide the changes from the site
+		inst.toString = () => bak;
+
+		return inst;
+	},
+});
 
 Reflect.get = new Proxy(Reflect.get, {
 	apply(target, that, args) {
