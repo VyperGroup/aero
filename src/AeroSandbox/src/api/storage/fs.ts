8d9355120c4e2d49491da77fbbbc5cba6ec14c56
Ryan Wilson <inbox@ryanwilson.space>
2024-01-16 23:28:23 -0500

    Add a stub for FS APIs

--- src/AeroSandbox/src/api/storage/fs.ts
+++ src/AeroSandbox/src/api/storage/fs.ts
@@ -1,1 +1,20 @@
 // TODO: Make it so that any of the directory names inside of the file system APIs are escaped with ${PROXY_ORIGIN}/${ORIGINAL VALUE}
+
+FileSystemDirectoryHandle.prototype.getDirectory = new Proxy(
+	FileSystemDirectoryHandle.prototype.getDirectory,
+	{
+		apply: () => {
+			return Reflect.apply(...arguments);
+			// TODO: Implement this stub
+		},
+	}
+);
+FileSystemDirectoryHandle.prototype.setDirectory = new Proxy(
+	FileSystemDirectoryHandle.prototype.setDirectory,
+	{
+		apply: () => {
+			return Reflect.apply(...arguments);
+			// TODO: Implement this stub
+		},
+	}
+);
