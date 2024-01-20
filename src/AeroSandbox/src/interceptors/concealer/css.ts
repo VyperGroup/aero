26fa05d1dbe6ecb0330eb0f4d71ac747eba377fa
Ryan Wilson <inbox@ryanwilson.space>
2024-01-20 17:50:17 -0500

    Fix some shadow root stylesheet concealers

--- src/AeroSandbox/src/interceptors/concealer/css.ts
+++ src/AeroSandbox/src/interceptors/concealer/css.ts
@@ -1,31 +1,53 @@
+/** Concealers for methods that return `CSSStyleSheet` (shadow root stylesheets)
+ * This file should be required into a bundle for AeroSandbox, so there are no exports
+ *
+ * @author Ryan Wilson
+ * @license AGPL-3.0-or-later
+ *
+ * @see https://drafts.csswg.org/cssom/#dom-documentorshadowroot-stylesheets
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet#obtaining_a_stylesheet - These explain all of the methods of obtaining `CSSStyleSheet`. TODO: Finish intercepting all of those revealers.
+ */
+
 import afterPrefix from "$aero/shared/afterPrefix";
 
-// Interceptors for Stylesheets
+// Proxy the getters for shadow root stylesheets
 
-function getSheet(sheet: HTMLLinkElement) {
-	if (sheet.href) sheet.href = afterPrefix(sheet.href);
-	// TODO: I don't know what is going on here lol
-	if (sheet.hasAttribute("parentStyleSheet"))
-		sheet.setAttribute(
-			"parentStyleSheet",
-			getSheet(sheet.getAttribute("parentStyleSheet")),
-		);
-	return sheet;
+function getSheet(sheet: CSSStyleSheet): CSSStyleSheet {
+	return new Proxy(sheet, {
+		get(target, prop: keyof CSSStyleSheet) {
+			if (prop === "href") {
+				return afterPrefix(target[prop]);
+			} else if (prop === "parentStyleSheet") {
+				// Parent recursion
+				const parentStyleSheet = target[prop];
+				if (parentStyleSheet !== null)
+					return getSheet(parentStyleSheet);
+			}
+			return target[prop];
+		},
+	});
 }
 
-/*
+// TODO: Inside of .xsl files spoof the conceal processing instructions nodes to hide their stylesheets
+function getProcessingInstructionSheet(
+	processingInstruction: ProcessingInstruction
+): ProcessingInstruction {
+	return new Proxy(processingInstruction, {
+		get(target, prop: keyof ProcessingInstruction) {
+			if (prop === "sheet") {
+				const sheet = target[prop];
+				if (sheet !== null) return getSheet(sheet);
+			}
+			return target[prop];
+		},
+	});
+}
+
 Object.defineProperty(document, "styleSheets", {
 	get: () => {
-		// StyleSheetList is read-only so the getter itself needs to be proxified
-		ret.item = new Proxy(ret.item, {
-			apply() {
-				return getSheet(Reflect.apply(target, that, args));
-			},
-		});
+		// Conceal each `CSSStyleSheet` from the `StyleSheetList`
+		const ret = Array.from(document.styleSheets).map(getSheet);
 
 		return ret;
 	},
 });
-*/
-
-// TODO: Support XML ProcessingInstruction.sheet
--- src/AeroSandbox/src/sandboxers/XML/intercept.xsl
+++ src/AeroSandbox/src/sandboxers/XML/intercept.xsl
@@ -2,6 +2,7 @@
     <xsl:text disable-output-escaping="yes">
         <![CDATA[
             // TODO: Inject all of the necessary things that would be injected in HTML
+            // TODO: Also, inject extra Element concealers for the XSL-specific nodes
         ]]>
     </xsl:text>
 </script>
