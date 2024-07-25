17386822a3dd568f5906b69af3728ce9238333d9
Ryan Wilson <inbox@ryanwilson.space>
2024-07-25 19:47:04 +0000

    Fix types (remove stray $)

--- src/AeroSandbox/types/$aero.d.ts
+++ src/AeroSandbox/types/$aero.d.ts
@@ -2,16 +2,16 @@ import type { AeroSandboxLogger } from "$shared/Loggers";
 import type BareClient from "@mercuryworkshop/bare-mux";
 
 export interface AeroGlobalType {
-	$sec: {
-		csp: string;
-		init: string;
-	};
-	$bc: BareClient;
-	logger: AeroSandboxLogger;
-	prefix: string;
+  sec: {
+    csp: string;
+    init: string;
+  };
+  bc: BareClient;
+  logger: AeroSandboxLogger;
+  prefix: string;
 }
 
 declare global {
-	// biome-ignore lint/style/noVar: <explanation>
-	var $aero: AeroGlobalType;
+  // biome-ignore lint/style/noVar: <explanation>
+  var $aero: AeroGlobalType;
 }
