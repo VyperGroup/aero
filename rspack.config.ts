3c2b7b5c67ec48ed162303cf033d7cd8ea429cb6
Ryan Wilson <inbox@ryanwilson.space>
2024-07-25 15:44:14 -0400

    Make aero NPM-ready

--- rspack.config.ts
+++ rspack.config.ts
@@ -4,45 +4,54 @@ import rspack from "@rspack/core";
 import { RsdoctorRspackPlugin } from "@rsdoctor/rspack-plugin";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
+const debugMode = process.env.DEBUG;
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const plugins: any[] = [
+	new CleanWebpackPlugin(),
+	new rspack.DefinePlugin({
+		// Webpack Feature Flags
+		FEATURE_CORS_EMULATION: JSON.stringify(false),
+		FEATURE_INTEGRITY_EMULATION: JSON.stringify(false),
+		FEATURE_ENC_BODY_EMULATION: JSON.stringify(false),
+		FEATURE_CACHES_EMULATION: JSON.stringify(false),
+		FEATURE_CLEAR_EMULATION: JSON.stringify(false),
+		REWRITER_HTML: JSON.stringify(false),
+		REWRITER_XSLT: JSON.stringify(false),
+		REWRITER_JS: JSON.stringify(false),
+		REWRITER_CACHE_MANIFEST: JSON.stringify(false),
+		SUPPORT_LEGACY: JSON.stringify(false),
+		SUPPORT_WORKER: JSON.stringify(false),
+		DEBUG: JSON.stringify(false)
+	})
+];
+
+if (debugMode)
+	plugins.push(
+		new RsdoctorRspackPlugin({
+			port: 3300
+		})
+	);
+
 export default {
-  mode: process.env.DEBUG ? "development" : "production",
-  entry: "./src/this/handle.ts",
-  plugins: [
-    new CleanWebpackPlugin(),
-    new RsdoctorRspackPlugin({
-      port: 3300,
-    }),
-    new rspack.DefinePlugin({
-      // Webpack Feature Flags
-      FEATURE_CORS_EMULATION: JSON.stringify(false),
-      FEATURE_INTEGRITY_EMULATION: JSON.stringify(false),
-      FEATURE_ENC_BODY_EMULATION: JSON.stringify(false),
-      FEATURE_CACHES_EMULATION: JSON.stringify(false),
-      FEATURE_CLEAR_EMULATION: JSON.stringify(false),
-      REWRITER_HTML: JSON.stringify(false),
-      REWRITER_XSLT: JSON.stringify(false),
-      REWRITER_JS: JSON.stringify(false),
-      REWRITER_CACHE_MANIFEST: JSON.stringify(false),
-      SUPPORT_LEGACY: JSON.stringify(false),
-      SUPPORT_WORKER: JSON.stringify(false),
-      DEBUG: JSON.stringify(false),
-    }),
-  ],
-  resolve: {
-    extensions: [".ts"],
-    tsConfigPath: path.resolve(__dirname, "./tsconfig.json"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: [/[\\/]node_modules[\\/]/],
-        loader: "builtin:swc-loader",
-      },
-    ],
-  },
-  output: {
-    filename: "sw.[hash].aero.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+	mode: debugMode ? "development" : "production",
+	entry: "./src/this/handle.ts",
+	plugins,
+	resolve: {
+		extensions: [".ts"],
+		tsConfigPath: path.resolve(__dirname, "./tsconfig.json")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: [/[\\/]node_modules[\\/]/],
+				loader: "builtin:swc-loader"
+			}
+		]
+	},
+	output: {
+		filename: "sw.aero.js",
+		path: path.resolve(__dirname, "dist")
+	}
 } as rspack.Configuration;
--- src/AeroSandbox/rspack.config.ts
+++ src/AeroSandbox/rspack.config.ts
@@ -1,14 +1,35 @@
-import { Dependencies } from "./node_modules/@rspack/core/dist/config/zod.d";
 import path from "path";
 import rspack from "@rspack/core";
 
 import { RsdoctorRspackPlugin } from "@rsdoctor/rspack-plugin";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
+const debugMode = process.env.DEBUG;
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const plugins: any[] = [
+	new CleanWebpackPlugin(),
+	new rspack.DefinePlugin({
+		// Webpack Feature Flags
+		// JS Rewriter
+		INCLUDE_ESNIFF: JSON.stringify(true),
+		INCLUDE_AST_PARSER_SEAFOX: JSON.stringify(true),
+		INCLUDE_AST_PARSER_OXC: JSON.stringify(false),
+		INCLUDE_AST_WALKER_TRAVERSE_THE_UNIVERSE: JSON.stringify(true)
+	})
+];
+
+if (debugMode)
+	plugins.push(
+		new RsdoctorRspackPlugin({
+			port: 3300
+		})
+	);
+
 export default {
-	mode: process.env.DEBUG ? "development" : "production",
+	mode: debugMode ? "development" : "production",
 	entry: {
-		aerosandbox: {
+		sandbox: {
 			import: "./build/init.ts",
 			dependsOn: "jsRewriter"
 		},
@@ -16,20 +37,7 @@ export default {
 			import: "./src/sandboxers/JS/JSRewriter.ts"
 		}
 	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new RsdoctorRspackPlugin({
-			port: 3300
-		}),
-		new rspack.DefinePlugin({
-			// Webpack Feature Flags
-			// JS Rewriter
-			INCLUDE_ESNIFF: JSON.stringify(true),
-			INCLUDE_AST_PARSER_SEAFOX: JSON.stringify(true),
-			INCLUDE_AST_PARSER_OXC: JSON.stringify(false),
-			INCLUDE_AST_WALKER_TRAVERSE_THE_UNIVERSE: JSON.stringify(true)
-		})
-	],
+	plugins,
 	resolve: {
 		extensions: [".ts"],
 		tsConfigPath: path.resolve(__dirname, "./tsconfig.json")
@@ -48,7 +56,7 @@ export default {
 		]
 	},
 	output: {
-		filename: "[name].[hash].aero.js",
+		filename: "[name].aero.js",
 		path: path.resolve(__dirname, "dist")
 	}
 } as rspack.Configuration;
