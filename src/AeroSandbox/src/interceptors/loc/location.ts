aae1d77af31df771ea3d1a028963227d0289f9c2
Ryan Wilson <inbox@ryanwilson.space>
2024-07-28 18:04:53 -0400

    Fix the location API interceptor

--- src/AeroSandbox/src/interceptors/loc/location.ts
+++ src/AeroSandbox/src/interceptors/loc/location.ts
@@ -1,15 +1,69 @@
-import locationProxy from "$shared/proxyLocation";
+import { APIInterceptor } from "../../../types/apiInterceptors";
+
+import { proxyLocation, upToProxyOrigin } from "$shared/proxyLocation";
 
 // Prevent detection by instanceof
-let inheritedObject = {};
-Reflect.setPrototypeOf(inheritedObject, Location.prototype);
+const inheritedObject = {};
+Reflect.setPrototypeOf(inheritedObject, Object.getPrototypeOf(location));
 
-window["$location"] = locationProxy;
-window["$location"]["Symbol.toStringTag"] = "Location";
+const wrap = (url: string) => $aero.config.prefix + url;
+// TODO: Set locationProxy as not writable and not configurable
 
-Object.defineProperty(document, "domain", {
-	get: () => $location.hostname
-});
-Object.defineProperty(document, "URL", {
-	get: () => $location.href
-});
+export default {
+	proxifiedObj: new Proxy(inheritedObject, {
+		get(target, prop) {
+			function internal() {
+				if (typeof target[prop] === "function") {
+					// @ts-ignore
+					// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+					const props: any = {
+						toString: () => proxyLocation().toString()
+					};
+
+					// These properties below are not defined in workers
+					if ("assign" in location)
+						props.assign = (url: string) =>
+							location.assign(wrap(url));
+					if ("replace" in location)
+						props.replace = (url: string) =>
+							location.replace(wrap(url));
+
+					return prop in props && prop in location
+						? props[prop]
+						: target[prop];
+				}
+
+				const fakeUrl = proxyLocation;
+
+				/**
+				 * `ancestorOrigins` is only found in Chrome
+				 */
+				const customProps = {
+					// TODO: Rewrite
+					//ancestorOrigins: location.ancestorOrigins,
+				};
+
+				if (prop in customProps) return customProps[prop];
+
+				if (prop in fakeUrl) return fakeUrl[prop];
+
+				return location[prop];
+			}
+
+			const ret = internal();
+
+			return ret;
+		},
+		set(target, prop, value) {
+			if (
+				prop === "pathname" ||
+				(prop === "href" && value.startsWith("/"))
+			)
+				target[prop] = upToProxyOrigin + value;
+			else target[prop] = value;
+
+			return true;
+		}
+	}),
+	globalProp: `["<proxyNamespace>"].proxifiedLocation`
+} as APIInterceptor;
--- src/AeroSandbox/src/shared/proxyLocation.ts
+++ src/AeroSandbox/src/shared/proxyLocation.ts
@@ -1,66 +1,17 @@
-import config from "../../../config";
+import sharedConfig from "./sharedConfig";
 
 import { afterPrefix } from "./getProxyUrl";
 
 const proxyLocation = () => new URL(afterPrefix(location.href));
-const upToProxyOrigin = () => config.prefix + proxyLocation().origin;
+const upToProxyOrigin = () => sharedConfig("prefix") + proxyLocation().origin;
 
-// Prevent detection by instanceof
-let inheritedObject = {};
-Reflect.setPrototypeOf(inheritedObject, Object.getPrototypeOf(location));
-
-const wrap = (url: string) => config.prefix + url;
-// TODO: Set locationProxy as not writable and not configurable
-const locationProxy = new Proxy(inheritedObject, {
-	get(target, prop) {
-		function internal() {
-			if (typeof target[prop] === "function") {
-				// @ts-ignore
-				const props: any = {
-					toString: () => proxyLocation().toString()
-				};
-
-				// These properties below are not defined in workers
-				if ("assign" in location)
-					props.assign = (url: string) =>
-						location["assign"](wrap(url));
-				if ("replace" in location)
-					props.replace = (url: string) =>
-						location["replace"](wrap(url));
-
-				return prop in props && prop in location
-					? props[prop]
-					: target[prop];
-			}
-
-			const fakeUrl = proxyLocation;
-
-			/**
-			 * `ancestorOrigins` is only found in Chrome
-			 */
-			const customProps = {
-				// TODO: Rewrite
-				//ancestorOrigins: location.ancestorOrigins,
-			};
-
-			if (prop in customProps) return customProps[prop];
-
-			if (prop in fakeUrl) return fakeUrl[prop];
-
-			return location[prop];
-		}
-
-		const ret = internal();
-
-		return ret;
-	},
-	set(target, prop, value) {
-		if (prop === "pathname" || (prop === "href" && value.startsWith("/")))
-			target[prop] = upToProxyOrigin + value;
-		else target[prop] = value;
-
-		return true;
-	}
+Object.defineProperty(document, "domain", {
+	// @ts-ignore
+	get: () => locationProxy.hostname
+});
+Object.defineProperty(document, "URL", {
+	// @ts-ignore
+	get: () => locationProxy.href
 });
 
-export { locationProxy as default, proxyLocation, upToProxyOrigin };
+export { proxyLocation, upToProxyOrigin };
