a8c951c491a986281bbebfa638af4b50575ef6b3
Ryan Wilson <inbox@ryanwilson.space>
2024-09-14 16:59:21 -0400

    Don't forget to run `npm install` and fix the directory check

--- deps.sh
+++ deps.sh
@@ -1,7 +1,9 @@
 # Setup the dev server
 org="https://github.com/VyperGroup/"
 
-if [ ! -d aero ]
+npm install > /dev/null
+
+if [ ! -d dev-server ]
 then
     git clone "${org}aero-dev-server.git" dev-server
 fi
